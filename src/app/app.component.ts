a146d08499138960fff1379ec3d48f16d5d247e4
cpena002 <cpena002@yahoo.com>
2019-11-06 21:56:58 -0800

    added a product-list component and began building the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,13 @@ import { Component } from '@angular/core';
 //The Component decorator. 
 @Component({
   selector: `pm-root`,
-  template: `<div><h1>{{pageTitle}}</h1><div>Additional Text</div></div>`
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>`
 })
 
 // The Component clasa
 export class AppComponent {
-  pageTitle: string = 'My Page Title';
+  pageTitle: string = 'Acme Product Management';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser'; // a Component?
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component'; //our app.component.ts file
+import { ProductListComponent } from './products/product-list.component'; //our app.component.ts file
 
-//@NgModule is a dcorative? <dramatic beaver>
+
+//@NgModule is a dcorative? 
 @NgModule({ // The properties are arrays.
-  declarations: [ AppComponent ],  // Defines which of our components belong to this module. 
+  declarations: [ AppComponent, ProductListComponent ],  // Defines which of our components belong to this module. 
   imports: [ BrowserModule ], // We defin external modules we want to have available
-  //for all of the components that belong to this module
-  // the BrowserModule is a module all Angular apps need to import. 
+  /* for all of the components that belong to this module
+     the BrowserModule is a module all Angular apps need to import. */
   bootstrap: [AppComponent] // Describes the startup component of the application
-  //The startup component should contain the selector we use in the index.html file.
+  /* The startup component should contain the selector we use in the index.html file. */
 })
 export class AppModule { }
 
