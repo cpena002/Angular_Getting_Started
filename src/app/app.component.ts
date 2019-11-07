cc868add9aae141788c05e92b3b6a3722290aa6c
cpena002 <cpena002@yahoo.com>
2019-11-06 20:44:11 -0800

    updating master cuz I locked myslef out at work...duh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+//The Component decorator. 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: `pm-root`,
+  template: `<div><h1>{{pageTitle}}</h1><div>Additional Text</div></div>`
 })
+
+// The Component clasa
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'My Page Title';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; // a Component?
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; //our app.component.ts file
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [AppComponent]
+//@NgModule is a dcorative? <dramatic beaver>
+@NgModule({ // The properties are arrays.
+  declarations: [ AppComponent ],  // Defines which of our components belong to this module. 
+  imports: [ BrowserModule ], // We defin external modules we want to have available
+  //for all of the components that belong to this module
+  // the BrowserModule is a module all Angular apps need to import. 
+  bootstrap: [AppComponent] // Describes the startup component of the application
+  //The startup component should contain the selector we use in the index.html file.
 })
 export class AppModule { }
+
+/* By convention, our root applicantion component, 
+    AppComponent, belongs to the applicaton's root Angular
+    module, AppModule, so we declare it here. */
