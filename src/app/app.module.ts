434353486626ca90bcb20ebb18e63380e0d66d1a
cpena002 <cpena002@yahoo.com>
2019-11-11 21:55:13 -0800

    created a nested component for the product list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component'; //our app.component.ts file
 import { ProductListComponent } from './products/product-list.component'; //our app.component.ts file
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StartComponent } from './shared/star.component';
 
 
 //@NgModule is a dcorative? 
 @NgModule({ // The properties are arrays.
-  declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe ],  // Defines which of our components belong to this module. 
+  declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe, StartComponent ],  // Defines which of our components belong to this module. 
   imports: [ BrowserModule, FormsModule ], // We defin external modules we want to have available
   /* for all of the components that belong to this module
      the BrowserModule is a module all Angular apps need to import. */
