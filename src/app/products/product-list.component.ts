8dc0734c4cebb2350476de77e67e876d2875f953
cpena002 <cpena002@yahoo.com>
2019-11-19 16:30:33 -0800

    Finalized the product service and subscription to same

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,6 +12,8 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
+
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
@@ -42,8 +44,13 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;            
+            },
+            error: err => this.errorMessage = err
+        });
     }
 
 }
